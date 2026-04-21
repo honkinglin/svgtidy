3097c023d6766d3e8802419b8f371f29d7a01bac
Kyrie Lin <kyrielin66@gmail.com>
2026-04-20 20:01:41 -0700

    feat: update plugins

--- src/plugins/collapse_groups.rs
+++ src/plugins/collapse_groups.rs
@@ -31,12 +31,18 @@ fn collapse_groups_in_nodes(nodes: &mut Vec<Node>, parent_name: &str) {
     let mut new_nodes = Vec::with_capacity(nodes.len());
 
     for node in nodes.drain(..) {
-        if let Node::Element(elem) = node {
-            if can_unwrap_group(&elem) {
-                // Unwrap: Move children to parent
+        if let Node::Element(mut elem) = node {
+            if can_move_group_id_to_child(&elem) {
+                let id = elem.attributes.shift_remove("id").unwrap();
+                let child = elem.children.pop().unwrap();
+
+                if let Node::Element(mut child_elem) = child {
+                    child_elem.attributes.insert("id".to_string(), id);
+                    new_nodes.push(Node::Element(child_elem));
+                }
+            } else if can_unwrap_group(&elem) {
                 new_nodes.extend(elem.children);
             } else {
-                // Keep
                 new_nodes.push(Node::Element(elem));
             }
         } else {
@@ -49,6 +55,25 @@ fn collapse_groups_in_nodes(nodes: &mut Vec<Node>, parent_name: &str) {
 
 fn can_unwrap_group(elem: &crate::tree::Element) -> bool {
     elem.name == "g" && elem.attributes.is_empty() && !has_group_semantic_children(elem)
+}
+
+fn can_move_group_id_to_child(elem: &crate::tree::Element) -> bool {
+    if elem.name != "g" || has_group_semantic_children(elem) {
+        return false;
+    }
+
+    if elem.attributes.len() != 1 || !elem.attributes.contains_key("id") {
+        return false;
+    }
+
+    if elem.children.len() != 1 {
+        return false;
+    }
+
+    matches!(
+        elem.children.first(),
+        Some(Node::Element(child_elem)) if !child_elem.attributes.contains_key("id")
+    )
 }
 
 fn has_group_semantic_children(elem: &crate::tree::Element) -> bool {
@@ -100,10 +125,26 @@ mod tests {
     #[test]
     fn test_keep_attr_group() {
         let input = "<svg><g id=\"g1\"><rect/></g></svg>";
-        let expected = "<svg><g id=\"g1\"><rect/></g></svg>";
+        let expected = "<svg><rect id=\"g1\"/></svg>";
         let mut doc = parser::parse(input).unwrap();
         CollapseGroups.apply(&mut doc);
         assert_eq!(printer::print(&doc), expected);
+    }
+
+    #[test]
+    fn test_keep_group_with_id_when_multiple_children() {
+        let input = "<svg><g id=\"g1\"><rect/><circle/></g></svg>";
+        let mut doc = parser::parse(input).unwrap();
+        CollapseGroups.apply(&mut doc);
+        assert_eq!(printer::print(&doc), input);
+    }
+
+    #[test]
+    fn test_keep_group_with_id_when_child_already_has_id() {
+        let input = "<svg><g id=\"g1\"><rect id=\"child\"/></g></svg>";
+        let mut doc = parser::parse(input).unwrap();
+        CollapseGroups.apply(&mut doc);
+        assert_eq!(printer::print(&doc), input);
     }
 
     #[test]
